c7381767300039ef318a551db78dd339abffa817
Epi Vou <pimenas@auvious.com>
2021-01-27 17:22:26 +0200

    Initial import

---
+++ SimpleConference/SimpleConference/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleConference
+//
+//  Created by Epimenidis Voutsakis on 27/1/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ SimpleConference/SimpleConference/SimpleConferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleConferenceApp.swift
+//  SimpleConference
+//
+//  Created by Epimenidis Voutsakis on 27/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleConferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
