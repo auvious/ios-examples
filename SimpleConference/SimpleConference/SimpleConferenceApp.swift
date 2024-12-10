1a81106a2c3326a95f9c11337516109fb04857c0
Michael Korkaris <michael.kork@auvious.com>
2024-12-10 14:00:55 +0200

    added handling for application going into background and back to foreground

--- SimpleConference/SimpleConference/SimpleConferenceApp.swift
+++ SimpleConference/SimpleConference/SimpleConferenceApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct SimpleConferenceApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             MainView()
